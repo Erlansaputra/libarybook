74b68461b1da6036e38b92a516a039d3e48b917f
Erlan Saputra <Erlansaputra2201@gmail>
2019-11-24 19:46:39 +0700

    bismillah

---
+++ db.php
@@ -0,0 +1,6 @@
+<!-- Postgresql connection -->
+<?php
+$dbconn =
+    pg_connect("host=localhost dbname=perpustakaan user=postgres password=Erlan2201")
+    or die('Could not connect: ' . pg_last_error());
+?>
